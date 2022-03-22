7a037be6baaa0f31ece2f8001350315bd859388b
AkashDKshirsagar <akash.kshirsagar@gmail.com>
2022-03-22 11:42:51 +0530

    day_10 completed

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,5 +1,12 @@
+class CatalogModel {
+  static final items = [
+    Item(01, "iPhone 12 pro", "Apple iPhone 12th Generation", 999, "#33505a",
+        "https://m.media-amazon.com/images/I/71MHTD3uL4L.jpg"),
+  ];
+}
+
 class Item {
-  final String id;
+  final int id;
   final String name;
   final String decs;
   final num price;
@@ -8,8 +15,3 @@ class Item {
 
   Item(this.id, this.name, this.decs, this.price, this.color, this.image);
 }
-
-final products = [
-  Item("A01", "iPhone 12 pro", "Apple iPhone 12th Generation", 999, "#33505a",
-      "https://m.media-amazon.com/images/I/71MHTD3uL4L.jpg"),
-];
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,8 @@
+import 'package:firstapp/models/catalog.dart';
 import 'package:flutter/material.dart';
 
 import '../widgets/drawer.dart';
+import '../widgets/item_widget.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -9,13 +11,20 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: Text("Catalog App"),
       ),
-      body: Center(
-        child: Container(
-          child: Text("Welcome to $days days of flutter by $name"),
+      body: Padding(
+        padding: const EdgeInsets.all(18.0),
+        child: ListView.builder(
+          itemCount: dummyList.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              item: dummyList[index],
+            );
+          },
         ),
       ),
       drawer: MyDrawer(),
